d0ad46aacc96332e98ace22a58f33b36e6ad0673
934622645 <934622645@qq.com>
2021-02-05 20:41:13 +0800

    2021-02-05

---
+++ build/CMakeFiles/ntestOne.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for ntestOne.
+# This may be replaced when dependencies are built.
---
+++ build/Lib/test/Src/CMakeFiles/test.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/haoge/Cxx/study/ntestOne/Lib/test/Src/test.cpp" "/home/haoge/Cxx/study/ntestOne/build/Lib/test/Src/CMakeFiles/test.dir/test.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "test_EXPORTS"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../Lib/test/Include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
