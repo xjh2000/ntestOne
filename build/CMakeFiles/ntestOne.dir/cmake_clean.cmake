c552eade2ab239562513372c983882438a69a1e3
934622645 <934622645@qq.com>
2021-02-06 11:16:42 +0800

    2020-02-06

---
+++ build/CMakeFiles/ntestOne.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ntestOne.dir/Core/Src/main.cpp.o"
+  "ntestOne"
+  "ntestOne.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ntestOne.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
